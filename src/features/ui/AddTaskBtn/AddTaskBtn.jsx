6da157dd4858faef4f2573f43a3a6b0ee1da32f0
Daniel Shevel <seveldaiil@gmail.com>
2025-12-09 15:46:57 +0300

    AddTaskBtn styles

--- src/features/ui/AddTaskBtn/AddTaskBtn.jsx
+++ src/features/ui/AddTaskBtn/AddTaskBtn.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useRef, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import styles from "./AddTaskBtn.module.scss";
 
 export function AddTaskBtn() {
+
+    const [isInputOpen, setIsInputOpen] = useState(false)
+    const InputRef = useRef(null)
+    const handle_setIsInputOpen = () => {
+        setIsInputOpen(!isInputOpen)
+    }
+
+
+    useEffect(() => {
+        if (isInputOpen) {
+            InputRef.current.focus()
+        }
+    }, [isInputOpen])
+
     return (
-        <button className={styles.AddTaskBtn}>
-            <span><p>Create new task</p><FaPlus /></span>
+        <button className={styles.AddTaskBtn} onClick={() => { handle_setIsInputOpen() }}>
+            <span style={{ display: isInputOpen ? 'none' : 'flex' }} className={styles.AddTaskBtn_text}><p>Create new task</p><FaPlus /></span>
+
+            <span style={{ display: isInputOpen ? 'block' : 'none' }} className={styles.AddTaskBtn_input}><input ref={InputRef} type="text" placeholder="Eneter task name:" /></span>
         </button>
     );
 }
